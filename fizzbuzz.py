9d840e69a3662f37db43d9c20e8d71a6018e6d0b
KSC <ksc1008@naver.com>
2024-06-04 20:28:51 +0900

    fix: changed end character of print

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,6 +4,6 @@ for num in range(1, end_number+1) :
     if num % 3 == 0 :
         print('fizz', end=' ')
     elif num % 5 == 0 :
-        print('buzz', end='')
+        print('buzz', end=' ')
     else :
         print(num, end=' ')
