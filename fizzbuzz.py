da1e636016d160434a7a49f63a9aa7ffdba77541
KSC <ksc1008@naver.com>
2024-06-04 20:25:45 +0900

    feat: add buzz with if statement

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,5 +3,7 @@ end_number = 20
 for num in range(1, end_number+1) :
     if num % 3 == 0 :
         print('fizz', end=' ')
+    if num % 5 == 0 :
+        print('buzz', end='')
     else :
         print(num, end=' ')
